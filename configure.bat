161a273fbc37c91541a2f985ae36ccd6a66ec30b
corrupted-application <96342169+corrupted-application@users.noreply.github.com>
2026-02-14 12:46:47 +0100

    Update and rename configure.py to configure.bat

---
+++ configure.bat
@@ -0,0 +1,11 @@
+@echo off
+title CorruptedApplication Timer Configure
+cls
+color 0a
+echo Welcome to the configuration wizard
+pause
+pip install win11toast rich
+cls
+echo Installing dependencies completed
+pause
+exit
