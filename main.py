5d4b676f105939f08b9d636264c23268001cf1ae
corrupted-application <96342169+corrupted-application@users.noreply.github.com>
2026-02-14 12:41:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,17 +10,24 @@ def cls():
 def title(title):
     os.system('title ' + title)
 
-cls()
-title("CorruptedApplication Timer")
-print("[yellow]Welcome to Corrupted Application Timer!\n")
-wait = Prompt.ask("[blue]Type when to ring (in seconds)")
-cls()
-print("Idle. Will ring in " + str(wait) + " seconds.")
-time.sleep(int(wait))
-cls()
-print("Ring ring ring")
-toast('The timer is up!')
-print("Quitting in 5 seconds...")
-time.sleep(5)
+while True:
+ cls()
+ title("CorruptedApplication Timer")
+ print("[yellow]Welcome to Corrupted Application Timer!\n")
+ wait = Prompt.ask("[blue]Type when to ring (in seconds)")
+ cls()
+ print("Idle. Will ring in " + str(wait) + " seconds.")
+ time.sleep(int(wait))
+ cls()
+ print("Ring ring ring")
+ toast('The timer is up!')
+ time.sleep(3)
+ quitting = Prompt.ask("[blue]Do you want to quit? (y/n)")
+ if quitting == "y":
+     break
+     cls()
+     exit()
+ elif quitting == "n":
+     cls()
 cls()
 exit()
